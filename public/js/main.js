05061e47255816bec617d2a90cb9da3c672d99fa
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-24 12:22:47 +0300

    create chat

--- public/js/main.js
+++
@@ -1,4 +0,0 @@
-const socket=  io();
-socket.on('message',message =>{
-    console.log(message);
-})
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const path =require('path');
-const http =require('http');
-const express =require('express');
-const socketio =require('socket.io');
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
-const app=express();
-const server=http.createServer(app);
-const io =socketio(server);
-
-//set static
-app.use(express.static(path.join(__dirname,'public')));
-
-//run
-io.on('connection', socket =>{
-     console.log('New WS Connection');
-     socket.emit('message','Welcome to chart');
-});
-const PORT=3000|| process.env.PORT;
-app.listen(PORT, ()=>console.log('server running on port '+PORT));
+var app = express();
+app.use(bodyParser.urlencoded({extended: true})) 
+app.use(bodyParser.json()) 
+app.use(express.static(__dirname));
+var dbUrl = 'mongodb+srv://admin:123roro123@cluster0.e5llt.mongodb.net/messages?retryWrites=true&w=majority';
+mongoose.connect(dbUrl , (err) => { 
+    console.log('mongodb connected',err);
+ })
+ var Message = mongoose.model('Message',{ name : String, message : String})
+ app.get('/messages', (req, res) => {
+    Message.find({},(err, messages)=> {
+      res.send(messages);
+    })
+  });
+  app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) =>{
+      if(err)
+        sendStatus(500);
+      res.sendStatus(200);
+    })
+  });
+var server = app.listen(3000, () => {
+    console.log('server is running on port', server.address().port);
+   });
