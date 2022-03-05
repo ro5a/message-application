e74178857529f71c6dc5963680c9b0ce103fb766
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-03-05 16:41:49 +0300

    create chat

--- server.js
+++ server.js
@@ -1,29 +1,85 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+const APP_PORT = process.env.APP_PORT || 3000
+const app = http.createServer(requestHandler)
+// SOCKET.IO CHAT EVENT HANDLING
+const io = require('socket.io')(app, {
+    path: '/socket.io',
+  })
 
-var app = express();
-app.use(bodyParser.urlencoded({extended: true})) 
-app.use(bodyParser.json()) 
-app.use(express.static(__dirname));
-var dbUrl = 'mongodb+srv://admin:123roro123@cluster0.e5llt.mongodb.net/messages?retryWrites=true&w=majority';
-mongoose.connect(dbUrl , (err) => { 
-    console.log('mongodb connected',err);
- })
- var Message = mongoose.model('Message',{ name : String, message : String})
- app.get('/messages', (req, res) => {
-    Message.find({},(err, messages)=> {
-      res.send(messages);
+ 
+
+app.listen(APP_PORT)
+console.log(`🖥 HTTP Server running at ${APP_PORT}`)
+
+// handles all http requests to the server
+function requestHandler(request, response) {
+  console.log(`🖥 Received request for ${request.url}`)
+  // append /client to serve pages from that folder
+  var filePath = './client' + request.url
+  if (filePath == './client/') {
+    // serve index page on request /
+    filePath = './client/index.html'
+  }
+  var extname = String(path.extname(filePath)).toLowerCase()
+  console.log(`🖥 Serving ${filePath}`)
+  var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+  }
+  var contentType = mimeTypes[extname] || 'application/octet-stream'
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      if (error.code == 'ENOENT') {
+        fs.readFile('./client/404.html', function (error, content) {
+          response.writeHead(404, { 'Content-Type': contentType })
+          response.end(content, 'utf-8')
+        })
+      } else {
+        response.writeHead(500)
+        response.end('Sorry, there was an error: ' + error.code + ' ..\n')
+      }
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType })
+      response.end(content, 'utf-8')
+    }
+  })
+}
+
+  var users = {}
+
+io.on('connection', (socket) => {
+  console.log('👾 New socket connected! >>', socket.id)
+
+  // handles new connection
+  socket.on('new-connection', (data) => {
+    // captures event when new clients join
+    console.log(`new-connection event received`, data)
+    // adds user to list
+    users[socket.id] = data.username
+    console.log('users :>> ', users)
+    // emit welcome message event
+    socket.emit('welcome-message', {
+      user: 'server',
+      message: `Welcome to this Socket.io chat ${data.username}. There are ${
+        Object.keys(users).length
+      } users connected`,
     })
-  });
-  app.post('/messages', (req, res) => {
-    var message = new Message(req.body);
-    message.save((err) =>{
-      if(err)
-        sendStatus(500);
-      res.sendStatus(200);
+  })
+
+  // handles message posted by client
+  socket.on('new-message', (data) => {
+    console.log(`👾 new-message from ${data.user}`)
+    // broadcast message to all sockets except the one that triggered the event
+    socket.broadcast.emit('broadcast-message', {
+      user: users[data.user],
+      message: data.message,
     })
-  });
-var server = app.listen(3000, () => {
-    console.log('server is running on port', server.address().port);
-   });
+  })
+})
